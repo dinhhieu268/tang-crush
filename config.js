4f587c05e0db84b7eacc970d4e90dc55103e0446
BinhMinhFPLHN <62173918+BinhMinhFPLHN@users.noreply.github.com>
2021-06-05 17:58:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anhhh <33',
+    desc: 'Chấp nhận tình cảm của anh đi không chạy đư đâu :3 ',
+    btnYes: 'Vẫn cứ là thích anhhh <333',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/100006087700776' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
