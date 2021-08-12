a9a88ca845dab2961b618c4fb59571b5c20697a9
dinhhieu268 <88796897+dinhhieu268@users.noreply.github.com>
2021-08-12 13:02:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Thư yêu dấu à!',
+    introTitle: 'slow yêu dấu !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
